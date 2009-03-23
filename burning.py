5b8a9b088513f6dcabf9c032d4d813f9d6143389
Antti Rasinen <ars@iki.fi>
2009-03-23 22:45:24 +0200

    Added logging basics

--- burning.py
+++ burning.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import
 
+import logging
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from handlers import urls
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 application = webapp.WSGIApplication(urls, debug=True)
 run_wsgi_app(application)
